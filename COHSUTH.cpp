7633ad0bc08aa3f0afec7f71d13be391a224bfe8
MaDDogx <ani101098@yahoo.in>
2019-09-23 11:49:36 +0530

    Update COHSUTH.cpp
    Working, I guess.

--- COHSUTH.cpp
+++ COHSUTH.cpp
@@ -1,6 +1,7 @@
 //C++ program to find category of line using Cohen-Sutherland
 #include<iostream.h>
 #include<conio.h>
+#include<graphics.h>
 
 const int INSIDE = 0; // 0000
 const int LEFT = 1;   // 0001
@@ -12,7 +13,7 @@ const int TOP = 8;    // 1000
 // bounded diagonally by (xmin, ymin), and (xmax, ymax)
 
 // xmax, xmin, ymax and ymin are global constants.
-const int xmin = 100, ymin = 100, xmax = 300, ymax = 300;
+const int xmin = 200, ymin = 150, xmax = 400, ymax = 300;
 
 int ComputeOutCode(double x, double y) {
 	int code;
@@ -38,35 +39,101 @@ void CohenSutherlandLineClip(double x0, double y0, double x1, double y1) {
 	// compute outcodes for P0, P1, and whatever point lies outside the clip rectangle
 	int outcode0 = ComputeOutCode(x0, y0);
 	int outcode1 = ComputeOutCode(x1, y1);
+	int accept = 0;
 
-	cout << "Clipping line from (" << x0 << ", " << y0 << ") to (";
-	cout << x1 << ", " << y1 << ")" << endl;
+	while (1) {
+		if (!(outcode0 | outcode1)) {
+			// bitwise OR is 0: both points inside window; trivially accept and exit loop
+			accept = 1;
+			break;
+		} else if (outcode0 & outcode1) {
+			// bitwise AND is not 0: both points share an outside zone (LEFT, RIGHT, TOP,
+			// or BOTTOM), so both must be outside window; exit loop (accept is false)
+			break;
+		} else {
+			// failed both tests, so calculate the line segment to clip
+			// from an outside point to an intersection with clip edge
+			double x, y;
 
-	if (!(outcode0 | outcode1)) {
-		// bitwise OR is 0: both points inside window; trivially accept and exit loop
-		// accept = true;
-		cout << "Line inside the window!" << endl;
-		// break;
-	} else if (outcode0 & outcode1) {
-		// bitwise AND is not 0: both points share an outside zone (LEFT, RIGHT, TOP,
-		// or BOTTOM), so both must be outside window; exit loop (accept is false)
-		cout << "Line outside the window!"  << endl;
-		// break;
-	} else {
-		// failed both tests, so calculate the line segment to clip
-		// from an outside point to an intersection with clip edge
-		cout << "Line partially inside the window! Clpping candidate!" << endl;
+			// At least one endpoint is outside the clip rectangle; pick it.
+			int outcodeOut = outcode0 ? outcode0 : outcode1;
+
+			// Now find the intersection point;
+			// use formulas:
+			//   slope = (y1 - y0) / (x1 - x0)
+			//   x = x0 + (1 / slope) * (ym - y0), where ym is ymin or ymax
+			//   y = y0 + slope * (xm - x0), where xm is xmin or xmax
+			// No need to worry about divide-by-zero because, in each case, the
+			// outcode bit being tested guarantees the denominator is non-zero
+			if (outcodeOut & TOP) {           // point is above the clip window
+				x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+				y = ymax;
+			} else if (outcodeOut & BOTTOM) { // point is below the clip window
+				x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+				y = ymin;
+			} else if (outcodeOut & RIGHT) {  // point is to the right of clip window
+				y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+				x = xmax;
+			} else if (outcodeOut & LEFT) {   // point is to the left of clip window
+				y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+				x = xmin;
+			}
+
+			// Now we move outside point to intersection point to clip
+			// and get ready for next pass.
+			if (outcodeOut == outcode0) {
+				x0 = x;
+				y0 = y;
+				outcode0 = ComputeOutCode(x0, y0);
+			} else {
+				x1 = x;
+				y1 = y;
+				outcode1 = ComputeOutCode(x1, y1);
+			}
+		}
+	}
+	if (accept) {
+		// Following functions are left for implementation by user based on
+		// their platform (OpenGL/graphics.h etc.)
+		// DrawRectangle(xmin, ymin, xmax, ymax);
+		// LineSegment(x0, y0, x1, y1);
+		line(xmin, 0, xmin, getmaxy());
+		line(xmax, 0, xmax, getmaxy());
+		line(0, ymin, getmaxx(), ymin);
+		line(0, ymax, getmaxx(), ymax);
+		settextstyle(0, HORIZ_DIR, 0);
+		outtextxy(xmin / 2, ymin / 2, "1001");
+		outtextxy(3 * xmin / 2, ymin / 2,"1000");
+		outtextxy(5 * xmin / 2, ymin / 2, "1010");
+		outtextxy(xmin / 2, 3 * ymin / 2, "0001");
+		outtextxy(3 * xmin / 2, 3 * ymin / 2, "0000");
+		outtextxy(5 * xmin / 2, 3 * ymin / 2, "0010");
+		outtextxy(xmin / 2, 5 * ymin / 2, "0101");
+		outtextxy(3 * xmin / 2, 5 * ymin / 2, "0100");
+		outtextxy(5 * xmin / 2, 5 * ymin / 2, "0110");
+		outtextxy(x0, y0, " <- First point.");
+		outtextxy(x1, y1, " <- Second point.");
+		line(x0, y0, x1, y1);
 	}
 }
 
 int main() {
-	clrscr();
-	cout << "Viewport dimensions:";
-	cout << " (" << xmin << ", " << ymin << ") to (";
-	cout << xmax << ", " << ymax << ")" << endl;
-	CohenSutherlandLineClip(110, 120, 250, 245);
-	CohenSutherlandLineClip(50, 50, 200, 50);
-	CohenSutherlandLineClip(80, 95, 125, 160);
+	int gd = DETECT, gm;
+
+	int x0, y0, x1, y1;
+
+	cout << "xmin = " << xmin << " xmax = " << xmax << endl;
+	cout << "ymin = " << ymin << " ymax = " << ymax << endl;
+
+	cout << "Enter coordinates of P0: ";
+	cin >> x0 >> y0;
+	cout << "Enter coordinates of P1: ";
+	cin >> x1 >> y1;
+
+	initgraph (&gd, &gm, "C:\\TURBOC3\\BGI");
+
+	CohenSutherlandLineClip(50, 80, 250, 250);
+
 	getch();
 	return 0;
 }
